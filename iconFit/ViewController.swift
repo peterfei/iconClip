b670f8c7137816fa7f2048dcb1c0b475917b9d37
peterfei <peterfeispace@gmail.com>
2016-12-14 10:17:52 +0800

    Initial Commit

---
+++ iconFit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iconFit
+//
+//  Created by peterfei on 2016/12/14.
+//  Copyright © 2016年 peterfei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
